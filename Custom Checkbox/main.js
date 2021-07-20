51c18441807853da10255550b9b839d6c2a6f484
Aakash deep Gupta <AakashdeepGupta18@gmail.com>
2021-07-20 19:42:29 +0530

    Added Custom Checkbox

---
+++ Custom Checkbox/main.js
@@ -0,0 +1,8 @@
+ let Check =document.getElementById('cb');
+ Check.onchange =function(){
+   if(Check.checked){
+     console.log(' i am checked')
+   }else{
+     console.log(' i am not checked')
+   }
+ }
